b8988753a5e97a58a3827b6571f8c035af90db42
Mushtaq Patel <mushtaqpatel0505@gmail.com>
2021-08-02 15:55:25 +0530

    Update app.py
    log of s and t

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import lightgbm as lgb
 
 #Initialize the flask App
 app = Flask(__name__)
-model = lgb.Booster(model_file='lgb_regressor.txt')
+model = lgb.Booster(model_file='lgb_regressorr.txt')
 
 #default page of our web-app
 @app.route('/')
@@ -21,9 +21,14 @@ def predict():
     '''
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features, num_iteration=model.best_iteration)
-
-    output = prediction
+    df = pd.DataFrame(final_features, columns = ['s','t'])
+    df['s'] = np.log(df['s'])
+    df['t'] = np.log(df['t'])
+    prediction = model.predict(df, num_iteration=model.best_iteration)
+    if prediction[0]>0.99:
+        output = 0.99
+    else:
+        output = prediction[0]
 
     return render_template('index.html', prediction_text='Prediction of top is : {}'.format(output))
 
